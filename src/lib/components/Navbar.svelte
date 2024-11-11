81bf8661cf4bc8574145682e6cb3c31ba1f7cbf4
Barry Sande <barrysandebbs@gmail.com>
2024-11-11 22:30:13 +0300

    final responsive nav

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,61 +3,81 @@
 	import { Hamburger } from 'svelte-hamburgers';
 
 	import { fly } from 'svelte/transition';
-	import { quadInOut } from 'svelte/easing';
-	import ProductCardList from './ProductCardList.svelte';
+	import { quadInOut, quadOut } from 'svelte/easing';
 
 	let open = $state(false);
-	let showMenu = $state(false);
+	let transitionParams = {
+		x: -320,
+		duration: 200,
+		easing: quadInOut
+	};
 </script>
 
-<nav class=" flex justify-around items-center gap-16 p-5 bg-charcoal text-white">
-	<div class=" mt-2 lg:hidden">
-		<Hamburger
-			bind:open
-			type="spring-r"
-			title="Toggle products menu"
-			ariaControls="nav"
-			--color="white"
-			--active-color="#FBAF85"
-			--padding="2px"
-			--layer-width="22px"
-			--layer-height=""
-			--layer-spacing="4px"
-		/>
+<nav
+	class="flex justify-between items-center bg-charcoal text-white px-6 lg:px-[7rem] py-3 md:py-5"
+>
+	<div class="flex gap-3 md:gap-8 justify-center items-center">
+		<div class="lg:hidden pt-2">
+			<Hamburger
+				bind:open
+				type="spring-r"
+				title="Toggle products menu"
+				ariaControls="nav"
+				--color="white"
+				--active-color="#FBAF85"
+				--padding="2px"
+				--layer-width="22px"
+				--layer-height=""
+				--layer-spacing="4px"
+			/>
+			{#if open}
+				<ul
+					class="flex flex-col gap-3"
+					transition:fly={{
+						delay: 100,
+						duration: 700,
+						easing: quadOut,
+						x: -30,
+						y: -15
+					}}
+				>
+					<li><a class="text-gray-100 hover:text-orange-bright uppercase" href="/">Home</a></li>
+					<li>
+						<a class="text-gray-100 hover:text-orange-bright uppercase" href="/headphones"
+							>Headphones</a
+						>
+					</li>
+					<li>
+						<a class="text-gray-100 hover:text-orange-bright uppercase" href="/speakers">Speakers</a
+						>
+					</li>
+					<li>
+						<a class="text-gray-100 hover:text-orange-bright uppercase" href="/earphones"
+							>Earphones</a
+						>
+					</li>
+				</ul>
+			{/if}
+		</div>
+		{#if !open}
+			<p class="text-xl font-bold md:text-3xl">audiophile</p>
+		{/if}
 	</div>
-	{#if !open}
-		<div class="text-xl font-bold md:text-2xl">audiophile</div>
-	{/if}
-	<div
-		class="flex mt-8 space-y-4 lg:flex lg:space-y-0 lg:flex-row items-center lg:space-x-10 lg:mt-0 {showMenu
-			? 'flex'
-			: 'hidden'}"
-	>
-		<a class="text-gray-100 hover:text-orange-bright uppercase" href="/">Home</a>
-		<a class="text-gray-100 hover:text-orange-bright uppercase" href="/headphones">Headphones</a>
-		<a class="text-gray-100 hover:text-orange-bright uppercase" href="/speakers">Speakers</a>
-		<a class="text-gray-100 hover:text-orange-bright uppercase" href="/earphones">Earphones</a>
-	</div>
+
+	<ul class="lg:flex gap-6 hidden p-5">
+		<li><a class="text-gray-100 hover:text-orange-bright uppercase" href="/">Home</a></li>
+		<li>
+			<a class="text-gray-100 hover:text-orange-bright uppercase" href="/headphones">Headphones</a>
+		</li>
+		<li>
+			<a class="text-gray-100 hover:text-orange-bright uppercase" href="/speakers">Speakers</a>
+		</li>
+		<li>
+			<a class="text-gray-100 hover:text-orange-bright uppercase" href="/earphones">Earphones</a>
+		</li>
+	</ul>
 
 	{#if !open}
 		<ShoppingCart />
-	{/if}
-	{#if open}
-		<ul
-			class="flex flex-col gap-4 justify-center items-center"
-			transition:fly|global={{ y: -15, duration: 500, easing: quadInOut }}
-		>
-			<li><a class="text-gray-100 hover:text-orange-bright uppercase" href="/">Home</a></li>
-			<li>
-				<a class="text-gray-100 hover:text-orange-bright uppercase" href="/headphones">Headphones</a
-				>
-			</li>
-			<li>
-				<a class="text-gray-100 hover:text-orange-bright uppercase" href="/speakers">Speakers</a>
-			</li>
-			<li>
-				<a class="text-gray-100 hover:text-orange-bright uppercase" href="/earphones">Earphones</a>
-			</li>
-		</ul>
 	{/if}
 </nav>
