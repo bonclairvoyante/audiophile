1bbfefb38ba5c017c49f359e61367039b3bd6b07
Barry Sande <barrysandebbs@gmail.com>
2024-11-16 03:57:02 +0300

    .

--- src/routes/(categories)/earphones/[earphonesId]/+page.ts
+++ src/routes/(categories)/earphones/[earphonesId]/+page.ts
@@ -2,15 +2,12 @@ import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ fetch}) => {
-	try {
-		const response = await fetch('/src/lib/data.json');
+	
+		const response = await fetch(`/src/lib/data.json`);
 		if (!response.ok) {
 			error(response.status);
 		}
 		const products = await response.json();
 		return { products };
-	} catch (error) {
-		console.error(error);
-		return { error: 'Unable to fetch products' };
-	}
+	
 };
